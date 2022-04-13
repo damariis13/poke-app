e63b11e72fb0eabc9bdddcf509cc5b1df30a4491
damaris <damarisdallorto@outlook.com>
2022-04-13 01:45:01 -0500

    adding search component

--- components/search/index.jsx
+++ components/search/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import Image from 'next/image';
 import styles from './search.module.scss';
+import searchIcon from '../../assets/search-icon.svg';
+import searchIcon1 from '../../assets/search-regular-24.png'
 
 const Search = () => {
     return (
         <div className={styles.search}>
-            Hola
+            <Image 
+            src={searchIcon}
+            alt="search-img"
+            />
+            <input type="text" placeholder="Buscar pokemon" />
         </div>
     )
 }
